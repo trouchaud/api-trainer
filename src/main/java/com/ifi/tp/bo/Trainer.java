7ef6348abb708d44ce226f07eeffd42328e537f1
Alexandre <trouchaud.alexandre@outlook.fr>
2018-12-15 01:52:36 +0100

    ADD: getter/setter id pour trainer

--- src/main/java/com/ifi/tp/bo/Trainer.java
+++ src/main/java/com/ifi/tp/bo/Trainer.java
@@ -34,6 +34,14 @@ public class Trainer {
         this.name = name;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
